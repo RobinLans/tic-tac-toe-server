cfd09b5d48bfef1d1380b0c00b29af8d1d0d5d1b
Robin Lans <roblan95@icloud.com>
2022-02-03 02:52:19 +0100

    test

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: "https://loving-goodall-298aab.netlify.app/",
+        origin: "https://loving-goodall-298aab.netlify.app",
         // "https://admiring-jang-94e13e.netlify.app",
         methods: ["GET", "POST"],
     },
